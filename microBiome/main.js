bb065810656191eacd783657b0cfb32cd41fbae5
olivier-jnk <oliviertrouvain01@gmail.com>
2024-01-30 14:27:05 +0100

    Ajout du systeme de stockage dans la memoire (objet) l'espece de la creature environnante et ajout du systeme d'action en fonction du type d'espece (accouplement (fonctionnel mais à regler), predation (encore qlq soucis à regler))

--- microBiome/main.js
+++ microBiome/main.js
@@ -7,10 +7,11 @@ setInterval(()=> {
     valeur2 = calculateRandomValue(-1,1)
 },1000)
 
-function Creature(specie, categorie, colorChoosen) {
+function Creature(specie, categorie, predateur, colorChoosen) {
     this.specie = specie;
     this.color = colorChoosen;
     this.categorie = categorie;
+    this.predateur = predateur; // definition du predateur de la creature // plus tard en set plusieurs.
     this.vitesseDeplacement = calculateRandomValue(5, 15);
     this.vie = calculateRandomValue(50, 100);
     this.degats = calculateRandomValue(10, 20);
@@ -33,9 +34,9 @@ function calculateRandomValue(min, max) {
 
 Creature.prototype.deplacer = function() {
     let id = this.id;
-    console.log(this.position.x + " position x")
-    if(this.position.x > 85){
-        console.log("x + 100")
+    // console.log(this.position.x + " position x")
+    if(this.position.x > 80){
+        // console.log("x + 100")
         posXDep = calculateRandomValue(-2,-1)
         posYDep = calculateRandomValue(-1,1)
     }
@@ -77,7 +78,9 @@ Creature.prototype.effectuerActions = function() {
     const creaturesAutour = creatures.filter(creature => {
         if (creature.id !== this.id) {
             const distance = this.calculerDistance(this.position, creature.position);
-            return distance < this.rayonPerimetre;
+            this.creatureProche = creature.specie; // envoie à la bille la creature qui rentre dans sa zone. // pb si plusieurs creatures.
+            // Possibilité d'utiliser cette methode pour stocker des infos dans la "memoire" de la bille.
+            return distance < this.rayonPerimetre; // return aussi la creature autour.
         }
         return false;
     });
@@ -97,11 +100,22 @@ Creature.prototype.effectuerActions = function() {
             this.deplacer();
         // function rechercher nourriture.
         }
-        
     }else{
         if(creaturesAutour.length > 0){ //predateurAuxAlentours // Fonctionnalitée completement buggé ! QUOIQUE ?
-            
-            console.log("Creature aux alentours !" + " cible = " + this.id + "creature = ")
+            console.log("creature aux alentours.")
+            // console.log("Creature aux alentours !" + " cible = " + this.id + "creature = " + this.creatureProche)
+            if(this.creatureProche === this.predateur){ // probleme ca reste ancré.
+                console.log(this.specie + " dit : Prédateur aux alentours. " + this.creatureProche)
+            }
+            if(this.creatureProche === this.specie){
+                console.log(this.specie + " animal de la meme espece dans les alentours. " + this.creatureProche)
+                if(this.feed > 70){
+                    // probleme de double accouplement (les deux font l'action)
+                    // faire en sorte de recup la feed des 2 + de faire en sorte que ce soit celui le plus nourri qui effectue
+                    // L'action.
+                    newCreature2(this.specie, this.categorie, this.predateur, this.color )
+                }
+            }
             this.deplacer(); // donner des instructions de deplacement spécifique en parametre OU function speciale de fuite.
         }
         else if(value > 0){ //PartenaireAuxAlentours && feed > 70 && PartenaireFeed > 70
@@ -121,23 +135,33 @@ Creature.prototype.calculerDistance = function(point1, point2) {
 };
 
 const creatures = [
-    new Creature("Creature1", "mobile","black"),
-    new Creature("Creature2", "mobile","red"),
-    new Creature("Creature3", "mobile","blue"),
+    new Creature("poule", "mobile","renard","grey"),
+    new Creature("poule", "mobile","vipere","grey"), // renard orange
+    new Creature("poule", "mobile","poule","grey"), // vipere green
 ];
 
 function newCreature() {
     let creature = prompt("Nom de la créature");
     let spe = prompt("Mobile ou immobile");
+    let predateur = prompt("predateur")
     let couleur = prompt("Couleur");
     
-    let newCreatureInstance = new Creature(creature, spe, couleur);
+    let newCreatureInstance = new Creature(creature, spe, predateur, couleur);
 
     creatures.push(newCreatureInstance);
 }
 
+function newCreature2(creature, spe, predateur, couleur) {
+    // plus tard set TOUS LES parametres.
+    // etablir un moyen pour cloner tous les attributs classiques de la creature parent.
+    // Faire en sorte que le nouveau né pop au meme endroit que le parent.
+    let newCreatureInstance = new Creature(creature, spe, predateur, couleur);
+
+    creatures.push(newCreatureInstance);
+} // regrouper les 2 plus tard !
+
 function popC(){
-    let newClassicC = new Creature ("classique","classique","pink")
+    let newClassicC = new Creature ("classique","classique", "poule","pink")
     creatures.push(newClassicC)
 }
 
