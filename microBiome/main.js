6344a1a2fbde5f2a968b4a79e7eddb8ad00c5ce9
olivier trouvain <oliviertrouvain01@gmail.com>
2024-02-02 23:17:03 +0100

    Systeme de detection de créatures environnantes fonctionnel et optimisé + accouplement optimal + Nettoyage.

--- microBiome/main.js
+++ microBiome/main.js
@@ -7,7 +7,7 @@ setInterval(()=> {
     valeur2 = calculateRandomValue(-1,1)
 },1000)
 
-function Creature(specie, categorie, predateur, colorChoosen) {
+function Creature(specie, categorie, predateur,sexe, colorChoosen) {
     this.specie = specie;
     this.color = colorChoosen;
     this.categorie = categorie;
@@ -15,10 +15,13 @@ function Creature(specie, categorie, predateur, colorChoosen) {
     this.vitesseDeplacement = calculateRandomValue(5, 15); // Innutilisé pour l'instant.
     this.vie = calculateRandomValue(50,100); //calculateRandomValue(50, 100)
     this.degats = calculateRandomValue(10, 20);
-    this.position = { x: calculateRandomValue(10,80), y: calculateRandomValue(10,80)};
+    this.position = { x: calculateRandomValue(10,20), y: calculateRandomValue(10,20)}; // 10, 80 normalement.
     this.rayonPerimetre = 15; // perimetre plutot bon. // peut etre plus.
     this.id = specie + categorie + calculateRandomValue(1,10000);
-    this.feed = 100; // 100
+    this.sexe = sexe;
+    this.mating = 0;
+    this.mating = 0;
+    this.feed = calculateRandomValue(80,100); // 100
     //mieux générer l'id pour éviter les paires.
     let creatureCreated = document.createElement("div")
     creatureCreated.className = "dot";
@@ -26,6 +29,7 @@ function Creature(specie, categorie, predateur, colorChoosen) {
     biome.appendChild(creatureCreated)
     let idC = document.getElementById(this.id)
     idC.style.backgroundColor = colorChoosen;
+
 }
 
 function calculateRandomValue(min, max) {
@@ -76,53 +80,53 @@ Creature.prototype.deplacer = function() {
 Creature.prototype.effectuerActions = function() {
 
     const creaturesAutour = creatures.filter(creature => {
-        if (creature.id !== this.id) {
+        if (creature.id !== this.id) { 
+
             const distance = this.calculerDistance(this.position, creature.position);
-            this.creatureProche = creature.specie; // envoie à la bille la creature qui rentre dans sa zone. // pb si plusieurs creatures.
-            // Possibilité d'utiliser cette methode pour stocker des infos dans la "memoire" de la bille.
-            return distance < this.rayonPerimetre; // return aussi la creature autour.
+            
+            let dist = distance < this.rayonPerimetre;
+
+            if(dist === true){
+                if(creature.specie === this.predateur){
+                    console.log(this.specie + " dit : Prédateur aux alentours. " + creature.specie)
+                }
+                else if(creature.specie === this.specie){
+                    console.log(this.specie + " animal de la meme espece dans les alentours. " + creature.specie)
+                    if(this.feed > 70 && creature.feed > 70 && this.sexe != creature.sexe && this.mating < 1 && creature.mating < 1){
+                        console.log("accouplement possible")
+
+                        // que faire si les deux feed sont au meme niveau ?
+                        // peut etre trouver un autre moyen. critere male/ femelle par exemple. ou indice superieur ou inferieur.
+                        // accouplement possible.
+                        if(this.feed < creature.feed){
+                            // rien faire (l'autre espece s'occupe d'effectuer l'action.)
+                        }
+                        else if(this.feed > creature.feed){
+                            // La dans ce cas, c'est la creature qui initie l'accouplement. 
+                            // methode pour eviter les double accouplements.
+                            newCreature2(calculateRandomValue(1,1000), this.categorie, "pouleJunior","male","#2F653E" )
+                            // faire plus tard en sorte que l'enfant pop, à côté des parents + qu'il ait
+                            // des spécificités à lui
+                            // + faire en sorte que plusieurs enfants puissent apparaitrent
+                            // et que les enfants ne puissent pas s'accoupler entre eux.
+                            // mettre un cooldown de reproduction apres ca.
+                            // faire en sorte que ne soit pas possible un double accouplement.
+                            // faire un systeme de grossesse ? 
+                        }
+                        this.mating = 1; // empeche les accouplements infinies.
+                    }
+                }
+                this.deplacer()
+
+            }else{
+                // console.log("false")
+                this.deplacer()
+            }
+            // fonctionnel ainsi et BEAUCOUP plus efficace car possibilité d'acceder au infos de la creature et de celle
+            // dans les alentours.
         }
         return false;
     });
-
-    // if(this.vies < 0){
-    //     console.log("c'est la mort !")
-    // }
-    // else if(this.feed < 40){ 
-    //     if(this.feed < 0){
-    //         setInterval(() =>{
-    //             console.log("la faim vous ronge" + this.id)
-    //             this.vies--
-    //             console.log("La faim vous retire de la vie.")
-    //         }, 5000)
-    //     }else{
-    //         console.log("La faim commence à se faire sentir.")
-    //         this.deplacer();
-    //     // function rechercher nourriture.
-    //     }
-    // }else{
-
-    if(creaturesAutour.length > 0){ //predateurAuxAlentours // Fonctionnalitée completement buggé ! QUOIQUE ?
-        console.log("creature aux alentours.")
-        // console.log("Creature aux alentours !" + " cible = " + this.id + "creature = " + this.creatureProche)
-        if(this.creatureProche === this.predateur){ // probleme ca reste ancré.
-            console.log(this.specie + " dit : Prédateur aux alentours. " + this.creatureProche)
-        }
-        if(this.creatureProche === this.specie){
-            console.log(this.specie + " animal de la meme espece dans les alentours. " + this.creatureProche)
-            if(this.feed > 70){
-                // probleme de double accouplement (les deux font l'action)
-                // faire en sorte de recup la feed des 2 + de faire en sorte que ce soit celui le plus nourri qui effectue
-                // L'action.
-                // patcher le probleme de reproduction infinie. nouveau né se reproduit...
-                newCreature2(calculateRandomValue(1,1000), this.categorie, "joe", "orange" )
-            }
-        }
-            this.deplacer(); // donner des instructions de deplacement spécifique en parametre OU function speciale de fuite.
-    }else{  
-        // errance
-        this.deplacer();
-    }
 };
 
 Creature.prototype.calculerDistance = function(point1, point2) {
@@ -132,9 +136,10 @@ Creature.prototype.calculerDistance = function(point1, point2) {
 };
 
 const creatures = [
-    new Creature("joe", "mobile","renard","grey"),
-    new Creature("jean", "mobile","vipere","grey"), // renard orange
-    new Creature("jack", "mobile","poule","grey"), // vipere green
+    new Creature("poule","mobile","renard","male","grey"),
+    new Creature("poule","mobile","renard","male","grey"),
+    new Creature("poule","mobile","renard","male","grey"),
+    new Creature("poule","mobile","renard","femelle","grey"),
 ];
 
 function newCreature() {
@@ -148,11 +153,11 @@ function newCreature() {
     creatures.push(newCreatureInstance);
 }
 
-function newCreature2(creature, spe, predateur, couleur) {
+function newCreature2(creature, spe, predateur,sexe, couleur) {
     // plus tard set TOUS LES parametres.
     // etablir un moyen pour cloner tous les attributs classiques de la creature parent.
     // Faire en sorte que le nouveau né pop au meme endroit que le parent.
-    let newCreatureInstance = new Creature(creature, spe, predateur, couleur);
+    let newCreatureInstance = new Creature(creature, spe, predateur,sexe, couleur);
 
     creatures.push(newCreatureInstance);
 } // regrouper les 2 plus tard ! ou modifier clairement le nom.
@@ -181,8 +186,6 @@ setInterval(() =>{
 
             // disparition totale un peu abrupte. faire fonctionner le systeme de setTimeout.
             
-            
-            
             // conflit j'ai l'impression quand suppression de deux creatures en mm temps. 
 
             // setTimeout(() => {
@@ -203,10 +206,17 @@ setInterval(() =>{
     console.log("retrait de nourriture général.")
 }, 5000)
 
+setInterval(() => {
+    for(i = 0; i < creatures.length; i++){
+        creatures[i].mating = 0;
+        // Pas la meilleure maniere car timer peu tomber juste apres un accouplement.
+        // trouver un moyen plus personnalisé à chaque creature.
+    }
+},10000)
+
 // async function CKill (KillCId){
     
 // }
 
-
 // faire une fonction speciale de kill creature. possible que à partir de l'id de la creature glissé en paramètre.
-// d'abord suppression de l'objet puis quelques secondes apres html.
+// d'abord suppression de l'objet puis quelques secondes apres element html.
